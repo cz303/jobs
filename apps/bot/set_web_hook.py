8d4f5924ca21c366d8b0eaf91a9c01be5de8cef4
Alexandr Sukhrin <alex.s@myualab.com>
2019-06-01 03:24:10 +0300

    update bot webhook

--- apps/bot/set_web_hook.py
+++ apps/bot/set_web_hook.py
@@ -6,13 +6,8 @@ from helpers.singleton import MetaSingleton
 class SetWebHook(metaclass=MetaSingleton):
     def __init__(self):
         self.bot = TeleBot(settings.TOKEN)
-        self.delete_hook()
-        self.set_hook()
-
-    def delete_hook(self):
         self.bot.delete_webhook()
 
-    def set_hook(self):
         self.bot.set_webhook(
             url=f"{settings.DOMAIN}/"
                 f"{settings.TOKEN}")
