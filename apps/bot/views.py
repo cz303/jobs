e287d8e5fa87de375968af8c69ee95450a40de60
Alexandr Sukhrin <alex.s@myualab.com>
2019-07-07 12:42:07 +0300

    fix response

--- apps/bot/views.py
+++ apps/bot/views.py
@@ -8,7 +8,8 @@ set_web_hook = SetWebHook()
 
 class BotJobsView(View):
     def post(self, request):
-        bot = set_web_hook.bot
-        Menu(request=request, bot=bot).send()
-
-        return HttpResponse(status=200)
+        try:
+            bot = set_web_hook.bot
+            Menu(request=request, bot=bot).send()
+        finally:
+            return HttpResponse(status=200)
